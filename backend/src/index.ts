dd814d8cd82c23ac879ba66513fab3d4526639ab
hasan-py <hasan29351@gmail.com>
2025-10-02 08:32:35 +0600

    (Get Started): backend- db, redis and some middlewares setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 import { Logger } from "@packages/logger";
 import express from "express";
+import { initializedDatabase } from "./config/dataSource";
+import redis, { CACHE_TIMES, generateCacheKey } from "./config/redisClient";
+import { corsConfig } from "./config/cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 class Server {
   public app: express.Application;
@@ -15,10 +20,33 @@ class Server {
       throw new Error("Environment variable `SERVER_PORT` not found");
 
     this.app.set("port", process.env.SERVER_PORT);
+
+    // Initialize the database connection
+    initializedDatabase();
+
+    // CORS configuration with emphasis on cookie support
+    this.app.use(corsConfig());
+
+    // Middleware for logging HTTP requests
+    this.app.use(morgan("tiny"));
+
+    // Add cookie parser middleware
+    this.app.use(cookieParser());
   }
 
   public routes(): void {
-    this.app.get("/", (req, res) => {
+    this.app.get("/", async (req, res) => {
+      const key = generateCacheKey("test");
+
+      let value = await redis.get(key);
+
+      if (!value) {
+        Logger.info(`Setting ${key} in Redis`);
+        await redis.set(key, "testValue", "EX", CACHE_TIMES.fifteenMinutes);
+      } else {
+        Logger.info(`Getting ${key} from Redis`);
+      }
+
       res.send("Hello World!");
     });
   }
